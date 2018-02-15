476160314d0d256eb7b3def8fc4ee207ded65030
Artaria <mahdi.niknami@gmail.com>
2018-02-15 15:53:51 +0330

    update class_test.php

--- class_test.php
+++ class_test.php
@@ -1,11 +1,13 @@
 <?php
-class Member{
+
+class Member
+{
     private $username = "";
-    public $islogined=false;
+    public $islogined = false;
 
     public function setUsername($username)
     {
-        $this->username=$username;
+        $this->username = $username;
     }
 
     public function getUsername()
@@ -15,21 +17,31 @@ class Member{
 
     public function login()
     {
-        $this->islogined=true;
+        $this->islogined = true;
     }
 
     public function logout()
     {
-        $this->islogined=false;
+        $this->islogined = false;
     }
 
     public function userState()
     {
-        $state=$this->islogined ? "logined" : "logout";
-        return $this->username." is ". ($this->islogined ? "logined" : "logout");
+        return $this->username . " is " . ($this->islogined ? "logined" : "logout");
     }
 }
-class Administrator extends Member{
+
+class Administrator extends Member
+{
+
+    public function setUsername($username)
+    {
+        echo "<br>this is admin<br>";
+        parent::setUsername($username);
+    }
+
+
+
     public function createArticle($title)
     {
         return "<br>the new article with title {$title} created by {$this->getUsername()}";
@@ -40,18 +52,19 @@ class Administrator extends Member{
         return "<br>the user with username {$user->getUsername()} baned with administrator";
     }
 }
+
 /*$user=new Member();
 $user->username = "mehdi";
 $user->login();
 var_dump($user);
 echo $user->userState();*/
-$user2=new Member();
-$user2->setUsername("ali");
 
-$admin=new Administrator();
+$user2 = new Member();
+$user2->setUsername("ali");
+echo $user2->userState();
+
+$admin = new Administrator();
 $admin->setUsername("admin");
-$admin->login();
 echo $admin->userState();
-echo $admin->createArticle("valentine");
-echo $admin->banUser($user2);
+
 
